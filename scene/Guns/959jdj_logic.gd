9ae1e0078ffe4eb1ac3c5545c575526835f42710
kendall_wlubel <polishwrobel@gmail.com>
2024-05-30 22:39:23 +0200

    959jdj dodane

---
+++ scene/Guns/959jdj_logic.gd
@@ -0,0 +1,11 @@
+extends "res://scene/Guns/gun_template.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	maxAmmo = 5
+	curAmmo = 5
+	maxMag = 3
+	curMag = 3
+
+
