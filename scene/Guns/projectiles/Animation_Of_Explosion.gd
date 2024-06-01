79a199ee1ffc6cf9f3607af6a46b8e1a21ef1b5d
kendall_wlubel <polishwrobel@gmail.com>
2024-06-01 11:24:17 +0200

    dodano wybuch rakiety

---
+++ scene/Guns/projectiles/Animation_Of_Explosion.gd
@@ -0,0 +1,11 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play("default")
+
+
+
+func _on_animation_finished():
+	queue_free()
--- script/Projectiles/rocket_projectile.gd
+++ script/Projectiles/rocket_projectile.gd
@@ -1,5 +1,7 @@
 extends "res://script/pila.gd"
 
+@onready var animation_of_explosion = preload("res://scene/Guns/projectiles/animated_explosion.tscn")
+
 
 func _ready():
 	SPEED = 500
@@ -7,4 +9,18 @@ func _ready():
 
 
 func _on_time_of_detenation_timeout():
+	queue_free()
+
+
+
+func _on_area_of_damage_body_entered(body):
+	if body.has_method("hit"):
+		body.hit(dmg)
+	else:
+		return 0
+	var animation_of_explosion_inst = animation_of_explosion.instantiate()
+	animation_of_explosion_inst.position = position
+	var projectiles = get_tree().get_first_node_in_group("projectiles")
+	projectiles.add_child(animation_of_explosion_inst)
+	
 	queue_free()
