405e3ef02b644ce3b199f6031ec804653b57c07a
kendall_wlubel <polishwrobel@gmail.com>
2024-06-01 11:55:35 +0200

    rakiety zabija kilka potworow

--- script/Projectiles/rocket_projectile.gd
+++ script/Projectiles/rocket_projectile.gd
@@ -1,6 +1,8 @@
 extends "res://script/pila.gd"
 
 @onready var animation_of_explosion = preload("res://scene/Guns/projectiles/animated_explosion.tscn")
+@onready var area_of_explosion = $Area_Of_Explosion
+
 
 
 func _ready():
@@ -14,10 +16,12 @@ func _on_time_of_detenation_timeout():
 
 
 func _on_area_of_damage_body_entered(body):
-	if body.has_method("hit"):
-		body.hit(dmg)
-	else:
-		return 0
+	for targets in area_of_explosion.get_overlapping_bodies():
+		if targets.has_method("hit"):
+			targets.hit(dmg)
+			print(targets)
+		else:
+			return 0
 	var animation_of_explosion_inst = animation_of_explosion.instantiate()
 	animation_of_explosion_inst.position = global_position
 	var projectiles = get_tree().get_first_node_in_group("projectiles")
